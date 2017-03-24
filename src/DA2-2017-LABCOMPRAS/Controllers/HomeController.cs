be237e40d6c8fc09d1df8a5ce66ea9bb26628411
150323017 <150323017@ESCE-D216-06>
2017-03-24 17:31:37 +0000

    Add project files.

---
+++ src/DA2-2017-LABCOMPRAS/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DA2_2017_LABCOMPRAS.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Entrada";
+            ViewBag.Texto = "Bem-Vindo à Página do Index";
+            return View();
+        }
+
+        public IActionResult Conceito()
+        {
+            ViewBag.Title = "Entrada da página Conceito";
+            ViewBag.Texto = "Um Novo Conceito";
+            return View();
+        }
+    }
+}
---
+++ src/DA2-2017-LABCOMPRAS/Models/Despesa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DA2_2017_LABCOMPRAS.Models
+{
+    public class Despesa
+    {
+        private string NomeDespesa;
+        private int Quantidade;
+        private double Valor;
+        private string Categoria;
+        private bool Pago;
+
+
+    
+
+
+
+
+    }
+}
