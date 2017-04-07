52d4d2479316f32fd33eaa40402c0bae222a0676
150323017 <150323017@ESCE-D216-06>
2017-04-07 17:12:32 +0100

    novaaula

---
+++ src/DA2-2017-LABCOMPRAS/Models/Repositorio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DA2_2017_LABCOMPRAS.Models
+{
+    public static class Repositorio
+    {
+        private static Despesa<Despesa> despesas = new Despesa<Despesa>();
+
+        public static Despesa<Despesa> Despesas
+        {
+            get
+            {
+                return Despesas;
+            }
+        }
+        public static void AddDespesa (Despesa newDespesa)
+        {
+            Despesas.Add(newDespesa);
+        }
+
+
+    }
+}
