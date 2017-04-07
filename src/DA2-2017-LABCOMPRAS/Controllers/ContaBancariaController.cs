aa14812bb2282ec477b77596e7c3c48ccf53b96c
150323017 <150323017@ESCE-D216-06>
2017-04-07 16:46:56 +0100

    novaaula

---
+++ src/DA2-2017-LABCOMPRAS/Controllers/ContaBancariaController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DA2_2017_LABCOMPRAS.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DA2_2017_LABCOMPRAS.Controllers
+{
+    public class ContaBancariaController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Texto = "Bem-Vindo ao site da Conta Bancária";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NovaContaBancaria()
+        {
+            ViewBag.Texto = "Bem-Vindo ao site da Conta Bancária!";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NovaContaBancaria(ContaBancaria obj)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("verificacao", obj);
+            }
+            else
+                return View();
+
+        }
+
+        public IActionResult Verificao()
+        {
+            ViewBag.Texto = "Confirme a sua Conta Bancária";
+            return View();
+        }
+
+    }
+}
---
+++ src/DA2-2017-LABCOMPRAS/Models/ContaBancaria.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DA2_2017_LABCOMPRAS.Models
+{
+    public class ContaBancaria
+    {
+        private string m_nometitular;
+        private int m_saldo;
+        private bool m_ativa;
+
+        [Required(ErrorMessage = "Por favor	introduza o Valor")]
+        public string NomeTitular
+        {
+            get { return m_nometitular; }
+            set { m_nometitular = value; }
+        }
+
+        [Required(ErrorMessage = "Por favor	introduza o Valor")]
+        public int Saldo
+        {
+            get { return m_saldo; }
+            set { m_saldo = value; }
+        }
+
+        public bool Ativa
+        {
+            get { return m_ativa; }
+            set { m_ativa = value; }
+        }
+
+
+        public ContaBancaria(string nome, int saldo, bool ativa)
+        {
+            m_nometitular = nome;
+            m_saldo = saldo;
+            m_ativa = ativa;
+        }
+
+        public ContaBancaria()
+        {
+
+        }
+
+    }
+}
