a08352a3906e300cd66284242193f60d8610321f
150323017 <150323017@ESCE-D216-06>
2017-04-21 17:56:35 +0100

    classe finalizada

--- src/DA2-2017-LABCOMPRAS/Models/WeatherApiResponse.cs
+++ src/DA2-2017-LABCOMPRAS/Models/WeatherApiResponse.cs
@@ -7,26 +7,29 @@ namespace DA2_2017_LABCOMPRAS.Models
 {
     public class WeatherApiResponse
     {
-        public apilocation Location
-        {
-            get { return Location; }
-            set { Location = value; }
 
-        }
-        public apicurrent Current
+        public apiLocation Location { get; set; }
+        public apiCurrent Current { get; set; }
+
+        public class apiLocation
         {
-            get { return Current; }
-            set { Current = value; }
+            public string Name { get; set; }
+            public string Region { get; set; }
+            public string Country { get; set; }
+            public double Lat { get; set; }
+            public double Lon { get; set; }
+            public string Tz_id { get; set; }
+            public int Localtime_epoch { get; set; }
+            public string Localtime { get; set; }
         }
 
-      public class apilocation
+        public class apiCurrent
         {
-          
+            public double Temp_c { get; set; }
+            public apiCondition condition { get; set; }
+            public class apiCondition
+            {
+                public string Text { get; set; }
+                public string Icon { get; set; }
+            }
         }
-
-        public class apicurrent
-        {
-
-        }
-    }
-}
